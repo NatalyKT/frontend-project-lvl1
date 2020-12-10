bdc93d1bdabb942bea428e3210f5a561a0692af2
Natalia <36547456+NatalyKT@users.noreply.github.com>
2020-12-10 19:39:42 +0300

    Update gcd.js
    change function name

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -3,18 +3,18 @@ import startGame from '../index.js';
 
 const description = 'Find the greatest common divisor of given numbers.';
 
-const getGreatestDivisor = (a, b) => {
+const getGreatestCommonDivisor = (a, b) => {
   if (!b) {
     return a;
   }
-  return getGreatestDivisor(b, a % b);
+  return getGreatestCommonDivisor(b, a % b);
 };
 
 const getQuestionAndCorrectAnswer = () => {
   const num1 = getRandomInRange(1, 10);
   const num2 = getRandomInRange(1, 10);
   return {
-    correctAnswer: String(getGreatestDivisor(num1, num2)),
+    correctAnswer: String(getGreatestCommonDivisor(num1, num2)),
     question: `${num1} ${num2}`,
   };
 };
